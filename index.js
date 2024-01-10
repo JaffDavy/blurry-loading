28e56afd284bec08242cdbc62bceef94fac38e89
Jaffdavy <jaffdavy100@gmail.com>
2024-01-10 11:18:55 +0100

    blurry image lint errors

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function blurring () {
   loadText.style.opacity = scale(load, 0, 100, 1, 0)
   bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
 }
-const scale = (num, inMin, inMax, outMin, outMax,) => {
+const scale = (num, inMin, inMax, outMin, outMax) => {
   return ((num - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin
 }
